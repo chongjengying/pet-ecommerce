64194dcbb3659ad034155649beeb00c33e545afa
jengying.c <jengying.chong@tiongnam.com.my>
2026-03-16 16:57:02 +0800

    Initial commit

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "https://rzzzmziuamvkqqlinzha.supabase.co"
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "sb_publishable_QESkvCPkmAPB-PM8ynyrqQ_vzvodKC5"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
